b1f09620a3e5d35dd6be0e45c3dab10d1b30f8c7
Haroun <hrnabd@hotmail.com>
2023-01-27 16:22:38 +0100

    commit before merge

--- js/addAnimal.js
+++ js/addAnimal.js
@@ -8,62 +8,76 @@ const form = document.querySelector('.modal form')
 
     const img = form[0].value
 
-    const description = form[2].value
+    const description = form[15].value
 
     const height = form[3].value
 
-    const weight = form[4].value
+    const weight = form[2].value
 
-    const speed = form[5].value
+    const speed = form[4].value
 
-    const lifeSpan = form[6].value
+    const lifeSpan = form[5].value
 
-    const gestationPeriod = form[7].value
+    const gestationPeriod = form[6].value
 
-  let location = ``
+    const dailySleep = form[7].value
+    const famousAnimal = form[14].value
+
+    const location = []
     const locationTab = form.querySelectorAll('input[name="location"]')
     locationTab.forEach(el => {
-      if(el.checked === true){
-        location += `<area>${el.value}</area>`
+      if(el.checked == true){
+        location.push(el.value) 
       }
     });
 
-    const famousAnimal = form[14].value
+ console.log(location)
 
-    const dailySleep = form[15].value
+    
 
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "./index.xml", true);
-    xhr.send();
+// Récupération du fichier XML
+    const xhrGet = new XMLHttpRequest();
+    xhrGet.open("GET", "./index.xml", true);
+    xhrGet.send();
     
      
-    xhr.onreadystatechange = function() {
+    xhrGet.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         
         const xml = this.responseXML;
         const root = xml.querySelector('root')
-     
-        root.innerHTML += `<species>
-        <name>${animalName}</name>
-        <img>${img}</img> 
-        <description>${description}</description>
-        <height unit="cm">${height}</height> 
-        <weight unit="cm">${weight}</weight>
-        <speed unit="km/h">${speed}</speed>
-        <lifeSpan unit="years">${lifeSpan}</lifeSpan>
-        <gestationPeriod unit="days">${gestationPeriod}</gestationPeriod>
-        <dailySleep unit="h">${dailySleep}</dailySleep>
-        <location>${location}</location>
-        <famousAnimal>${famousAnimal}</famousAnimal>
-        </species>`;   
-
-        console.log(xml)
-        
-          }
+     // Ajout du nouvel animal dans la racine fichier
+        const objectData = {name : animalName,
+        img : img,
+        description : description,
+        height : height,
+        weight : weight,
+        speed : speed,
+        lifeSpan: lifeSpan,
+        gestationPeriod : gestationPeriod,
+        dailySleep : dailySleep,
+        location : {area : location },
+        famousAnimal : famousAnimal
       }
 
 
+        console.log()
 
-}
-
+      // Envoyer la requête au script PHP 
+      // qui nous permettra de sauvegarder les données
+        const xhrPost = new XMLHttpRequest();
+        xhrPost.open("POST", "http://127.0.0.1:8000", true);
+        xhrPost.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhrPost.setRequestHeader("Access-Control-Allow-Origin", "*");
+        xhrPost.setRequestHeader("Sec-Fetch-Mode", "no-cors");
+        xhrPost.setRequestHeader("Sec-Fetch-Site", "cross-site");
+        xhrPost.onreadystatechange = function() {
+            if (xhrPost.readyState === 4 && xhrPost.status === 200) {
+                console.log(xhrPost.responseText);
+          
+            }
+        };
+        xhrPost.send(`data= ${JSON.stringify(objectData)}`);
+      }
+     }
+  }
